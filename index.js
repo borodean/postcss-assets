752f9bd7d2f35c466a5e71d2e95894e59ebf785f
Vadim Borodean <borodean@gmail.com>
2014-10-08 01:35:44 +0300

    Preserve hash and query params

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var postcss = require('postcss');
 
 var fs = require('fs');
 var path = require('path');
+var url = require('url');
 
 var R_ASSET = /asset\((.*?)([^\s'"]+)(.*?)\)/;
 
@@ -12,13 +13,14 @@ module.exports = function (options) {
     css.eachDecl(function (decl) {
       if (decl.value.indexOf('asset(') !== 0) return;
       var matches = decl.value.match(R_ASSET);
-      var resolvedPath;
+      var uri = url.parse(matches[2]);
+      var pathname = uri.pathname;
       var some = options.loadPaths.some(function (loadPath) {
-        resolvedPath = path.normalize('/' + loadPath + matches[2]);
-        return fs.existsSync(options.basePath + resolvedPath);
+        uri.pathname = path.normalize('/' + loadPath + pathname);
+        return fs.existsSync(options.basePath + uri.pathname);
       });
       if (!some) throw new Error;
-      decl.value = 'url(' + matches[1] + resolvedPath + matches[3] + ')';
+      decl.value = 'url(' + matches[1] + url.format(uri) + matches[3] + ')';
     });
   };
 };
