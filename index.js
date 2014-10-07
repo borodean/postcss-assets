f629d23bd1f817b12b89aaa35d4f457310614b94
Vadim Borodean <borodean@gmail.com>
2014-10-08 02:06:37 +0300

    Extract `resolve` method

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ var R_ASSET = /asset\((.*?)([^\s'"]+)(.*?)\)/;
 module.exports = function (options) {
   options.loadPaths = options.loadPaths || [];
   options.loadPaths.unshift('./');
+
+  function resolve(fn) {
+    var uri = url.parse(fn);
+    var pathname = uri.pathname;
+    var some = options.loadPaths.some(function (loadPath) {
+      uri.pathname = path.normalize('/' + loadPath + pathname);
+      return fs.existsSync(options.basePath + uri.pathname);
+    });
+    if (!some) throw new Error;
+    return url.format(uri);
+  }
+
   return function (css) {
     css.eachDecl(function (decl) {
       if (decl.value.indexOf('asset(') !== 0) return;
       var matches = decl.value.match(R_ASSET);
-      var uri = url.parse(matches[2]);
-      var pathname = uri.pathname;
-      var some = options.loadPaths.some(function (loadPath) {
-        uri.pathname = path.normalize('/' + loadPath + pathname);
-        return fs.existsSync(options.basePath + uri.pathname);
-      });
-      if (!some) throw new Error;
-      decl.value = 'url(' + matches[1] + url.format(uri) + matches[3] + ')';
+      decl.value = 'url(' + matches[1] + resolve(matches[2]) + matches[3] + ')';
     });
   };
 };
