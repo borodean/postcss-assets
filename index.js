77ad99a3351b3a95a8fc90f76ed8730842edeb70
Vadim Borodean <borodean@gmail.com>
2014-10-08 04:09:02 +0300

    Understand CSS escaping

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 var R_ASSET = /asset\((\s*['"]?)(.*?)(['"]?\s*)\)/;
+var R_ESCAPE = /\\(?:([0-9a-f]{1,6} ?)|(.))/g;
 var R_URL = /^([^\?#]+)(.*)/;
 
 module.exports = function (options) {
@@ -12,14 +13,21 @@ module.exports = function (options) {
 
   function resolve(fn) {
     var chunks = Array.prototype.slice.call(fn.match(R_URL), 1, 3);
-    var resolvedPathname;
+    var resolvedPath;
     var some = options.loadPaths.some(function (loadPath) {
-      resolvedPathname = path.normalize('/' + loadPath + chunks[0]);
-      return fs.existsSync(options.basePath + resolvedPathname);
+      resolvedPath = path.normalize('/' + loadPath);
+      return fs.existsSync(options.basePath + resolvedPath + unescape(chunks[0]));
     });
     if (!some) throw new Error;
-    chunks[0] = encodeURI(resolvedPathname);
+    chunks[0] = encodeURI(resolvedPath + chunks[0]).replace('%5C', '\\');
     return chunks.join('');
+  }
+
+  function unescape(string) {
+    return string.replace(R_ESCAPE, function (match, p1, p2) {
+      if (p1) return String.fromCharCode(p1);
+      return p2;
+    });
   }
 
   return function (css) {
