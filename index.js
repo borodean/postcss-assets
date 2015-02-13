ffd87dc43975aa595532f1f0c281405a4abb6813
Vadim Borodean <borodean@gmail.com>
2015-02-13 04:37:33 +0200

    Convert file extensions to `.es6`

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+require('6to5/register')({
+  // All the subsequent files required by node with the extension of `.es6`
+  // will be transformed to ES5
+  extensions: ['.es6']
+});
+
 module.exports = require('./lib/assets');
--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
+require('6to5/register')({
+  // All the subsequent files required by node with the extension of `.es6`
+  // will be transformed to ES5
+  extensions: ['.es6']
+});
+
 require('./lib/assets');
 require('./lib/mapFunctions');
 require('./lib/parseBytes');
