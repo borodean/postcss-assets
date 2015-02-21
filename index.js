68aa58b1a3a2aeff6bf5b7f4c8159a180da0fadb
Vadim Borodean <borodean@gmail.com>
2015-02-21 02:21:38 +0200

    `6to5` -> `babel`

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('6to5/register')({
+require('babel/register')({
   // All the subsequent files required by node with the extension of `.es6`
   // will be transformed to ES5
   extensions: ['.es6']
