8b8cb43526839713c4b83282eb16c5f915bd865c
Vadim Borodean <borodean@gmail.com>
2015-03-13 04:44:26 +0200

    Flatten `mapFunctions` test

--- test/map-functions.js
+++ test/map-functions.js
@@ -1,35 +1,34 @@
-// Vendor imports
+  // Vendor imports
 var expect = require('chai').expect;
 
 // Local imports
 var mapFunctions = require('../lib/map-functions');
 
-var MAP = {
-  decrease: function (params) {
-    return parseFloat(params, 10) - 1 + 'px';
-  },
-  increase: function (params) {
-    return parseFloat(params, 10) + 1 + 'px';
-  },
-  double: function (params) {
-    return parseFloat(params, 10) * 2 + 'px';
-  },
-  combine: function (a, b) {
-    return parseFloat(a, 10) + parseFloat(b, 10) + 'px';
-  }
-};
-
-function checkMapping(source, expectedResult) {
+function test(source, expectedResult) {
   return function () {
-    expect(mapFunctions(source, MAP)).to.equal(expectedResult);
+    var actualResult = mapFunctions(source, {
+      decrease: function (params) {
+        return parseFloat(params, 10) - 1 + 'px';
+      },
+      increase: function (params) {
+        return parseFloat(params, 10) + 1 + 'px';
+      },
+      double: function (params) {
+        return parseFloat(params, 10) * 2 + 'px';
+      },
+      combine: function (a, b) {
+        return parseFloat(a, 10) + parseFloat(b, 10) + 'px';
+      }
+    });
+    expect(actualResult).to.equal(expectedResult);
   };
 }
 
 describe('mapFunctions()', function () {
-  it('should map functions', checkMapping('increase(100px)', '101px'));
-  it('should map sibling functions', checkMapping('increase(100px), decrease(100px)', '101px, 99px'));
-  it('should map nested functions', checkMapping('double(increase(100px))', '202px'));
-  it('should skip unknown functions', checkMapping('unknown(100px)', 'unknown(100px)'));
-  it('should map inside unknown functions', checkMapping('unknown(increase(100px))', 'unknown(101px)'));
-  it('should accept multiple parameters', checkMapping('combine(20px, 15px)', '35px'));
+  it('should map functions', test('increase(100px)', '101px'));
+  it('should map sibling functions', test('increase(100px), decrease(100px)', '101px, 99px'));
+  it('should map nested functions', test('double(increase(100px))', '202px'));
+  it('should skip unknown functions', test('unknown(100px)', 'unknown(100px)'));
+  it('should map inside unknown functions', test('unknown(increase(100px))', 'unknown(101px)'));
+  it('should accept multiple parameters', test('combine(20px, 15px)', '35px'));
 });
