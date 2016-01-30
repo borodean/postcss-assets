8070c9f13209b80b1881fc83de027b3895656046
Vadym Borodin <borodean@gmail.com>
2016-01-30 15:34:46 +0200

    Remove word "should" from test descriptions

--- test/unescape-css.js
+++ test/unescape-css.js
@@ -1,14 +1,14 @@
 var test = require('ava');
 var unescapeCss = require('../lib/unescape-css');
 
-test('should unescape plain chars', function (t) {
+test('unescapes plain chars', function (t) {
   t.is(unescapeCss('Romeo \\+ Juliette'), 'Romeo + Juliette');
 });
 
-test('should unescape ASCII chars', function (t) {
+test('unescapes ASCII chars', function (t) {
   t.is(unescapeCss('\\34\\32'), '42');
 });
 
-test('should unescape Unicode chars', function (t) {
+test('unescapes Unicode chars', function (t) {
   t.is(unescapeCss('I \\2665  NY'), 'I ♥ NY');
 });
